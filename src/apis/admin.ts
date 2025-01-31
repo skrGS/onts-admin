af088eae73c63b1bdc4b4454255aa3c6e9de9242
skrGS <tselmen.ihelp@gmail.com>
2025-01-31 21:29:48 +0800

    fix

--- src/apis/admin.ts
+++ src/apis/admin.ts
@@ -1,8 +1,8 @@
 // /admin/chart-dashboard
 import { HttpRequest } from "@/utils/request";
 
-// const appHttpRequest = new HttpRequest(null, `https://onts.boosters.mn/admin`);
-const appHttpRequest = new HttpRequest(null, `http://localhost:3011/admin`);
+const appHttpRequest = new HttpRequest(null, `https://onts.boosters.mn/admin`);
+// const appHttpRequest = new HttpRequest(null, `http://localhost:3011/admin`);
 
 export const getChartDashboard = async () => {
   const res = await appHttpRequest.get(`/chart-dashboard`);
